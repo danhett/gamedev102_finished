df90fa25cf63d55abef559b03263d99115b5cb85
Dan <hellodanhett@gmail.com>
2017-03-14 12:14:07 +0000

    Rejigged and sorted base framework

---
+++ BlankGame/js/main.js
@@ -0,0 +1,17 @@
+var game;
+
+window.onload = function() {
+  setupPhaser();
+};
+
+function setupPhaser() {
+  game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameholder', {create: init});
+}
+
+function init() {
+  game.state.add("LoaderState", LoaderState);
+  game.state.add("MenuState", MenuState);
+  game.state.add("GameState", GameState);
+
+  game.state.start("LoaderState");
+}
---
+++ BlankGame/js/states/GameState.js
@@ -0,0 +1,9 @@
+var GameState = function(game){
+
+};
+
+GameState.prototype = {
+  create: function() {
+  	this.game.add.sprite(0, 0, "game-background");
+  }
+}
